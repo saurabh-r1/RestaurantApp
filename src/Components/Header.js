5e9fb6fb3857ee84829a81cfa72763c2a7bdab88
saurabh-r1 <sidlock2@gmail.com>
2023-08-14 11:21:04 +0530

    initial commits

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+const Header=props=>{
+    return(
+        <Fragment>
+            <header>YourMeals</header>
+            
+        </Fragment>
+    )
+};
+
+export default Header;
