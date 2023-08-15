8ba74118f6c5eab821c51f4f6ddb4f9c9bfd7807
saurabh-r1 <sidlock2@gmail.com>
2023-08-15 11:52:51 +0530

    created header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,28 @@
 import { Fragment } from "react";
+import "./Header.css";
+import HeaderImg from "./HeaderImg.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
-const Header=props=>{
-    return(
-        <Fragment>
-            <header>YourMeals</header>
-            
-        </Fragment>
-    )
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className="header">
+        <h1>ReactMeals</h1>
+        <div className="cart">
+        <div >
+          <FontAwesomeIcon icon={faCartShopping} />
+        </div>
+        <div>Your Cart</div>
+        <p id="count">0</p>
+        </div>
+        
+      </header>
+      <div className="main-image">
+        <img src={HeaderImg} alt="restra_header_img" />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Header;
