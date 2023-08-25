8e08e1366425eb5b1a6d027ec9d50b7d0686ab0c
saurabh-r1 <sidlock2@gmail.com>
2023-08-25 13:00:32 +0530

    finishing

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,18 +1,39 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from 'react';
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import "./HeaderCartButton.css";
-import CartContext from "../../store/cart-context";
+import CartContext from '../../store/cart-context';
+import './HeaderCartButton.css';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `button ${btnIsHighlighted ? "bump" : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className="button" onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className="icon">
         <FontAwesomeIcon icon={faCartShopping} />
       </span>
@@ -23,3 +44,4 @@ const HeaderCartButton = (props) => {
 };
 
 export default HeaderCartButton;
+
